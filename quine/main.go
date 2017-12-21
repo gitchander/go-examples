4f85cd7b58b46fb54e8f465eaf769dd14b5d1229
Chander <JPoChander@gmail.com>
2017-12-21 11:28:15 +0200

    fix quine

--- quine/main.go
+++ quine/main.go
@@ -3,17 +3,14 @@ package main
 import "fmt"
 
 func main() {
-
 	s := `package main
 
 import "fmt"
 
 func main() {
-	
 	s := %c%s%c
-	fmt.Printf(s, 96, s, 96)	
+	fmt.Printf(s, 96, s, 96)
 }
-
 `
 	fmt.Printf(s, 96, s, 96)
 }
